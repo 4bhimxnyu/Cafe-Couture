c557214b4d49533a1f4083b08cecfe0e76d53209
4bhimxnyu <120682491+4bhimxnyu@users.noreply.github.com>
2024-05-17 15:34:28 +0530

    Add files via upload

---
+++ cafe-courture/bootstap/main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Masaki Cafe Couture" , "Best Coffee Shop in th City." , "Finger licking good!!",],
+    typeSpeed: 40,
+    backSpeed: 30,
+    backDelay: 1000,
+    loop: true 
+})
